7abfd770d3174acc99daff2ef2b9a30b9fe6a84d
nikhildeepak <nikhildeepak8182@gmail.com>
2021-03-21 13:05:48 +0530

    1

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -4,36 +4,19 @@ class QuizBrain {
   int _questionNumber = 0;
 
   List<Question> _questionBank = [
-    Question('Some cats are actually allergic to humans', true),
-    Question('You can lead a cow down stairs but not up stairs.', false),
-    Question('Approximately one quarter of human bones are in the feet.', true),
-    Question('A slug\'s blood is green.', true),
-    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
-    Question('It is illegal to pee in the Ocean in Portugal.', true),
-    Question(
-        'No piece of square dry paper can be folded in half more than 7 times.',
-        false),
-    Question(
-        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
-        true),
-    Question(
-        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
-        false),
-    Question(
-        'The total surface area of two human lungs is approximately 70 square metres.',
-        true),
-    Question('Google was originally called \"Backrub\".', true),
-    Question(
-        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
-        true),
-    Question(
-        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
-        true),
+    Question('does my baby marry👰 me?', true),
+    Question('will my queen cry😭 today?', false),
+    Question('is \'Mahal\' an ANGEL😇?', true),
+    Question('property🥰 of mine?', true),
+    Question('is she my lobster👩 ', true),
+    Question('is she queen of heaven😍', true),
+    Question('is she cheating😡 on me', false),
   ];
 
   void nextQuestion() {
     if (_questionNumber < _questionBank.length - 1) {
       _questionNumber++;
+      print(_questionNumber);
     }
   }
 
@@ -45,9 +28,29 @@ class QuizBrain {
     return _questionBank[_questionNumber].questionAnswer;
   }
 
-  //TODO: Step 3 Part A - Create a method called isFinished() here that checks to see if we have reached the last question. It should return (have an output) true if we've reached the last question and it should return false if we're not there yet.
+  // Step 3 Part A - Create a method called isFinished() here that checks to see if we have reached the last question. It should return (have an output) true if we've reached the last question and it should return false if we're not there yet.
 
-  //TODO: Step 3 Part B - Use a print statement to check that isFinished is returning true when you are indeed at the end of the quiz and when a restart should happen.
+  bool isFinished() {
+    if (_questionNumber < _questionBank.length-1) {
+      print('isFinished is returnig false');
 
-  //TODO: Step 4 Part B - Create a reset() method here that sets the questionNumber back to 0.
+      return false;
+    }
+    else {
+      print('isFinished is returnig true');
+      return true;
+    }
+  }
+
+  //Step 3 Part B - Use a print statement to check that isFinished is returning true when you are indeed at the end of the quiz and when a restart should happen.
+
+  void reset() {
+    print(isFinished());
+
+    _questionNumber = 0;
+  }
+
+
+
+  // Step 4 Part B - Create a reset() method here that sets the questionNumber back to 0.
 }
